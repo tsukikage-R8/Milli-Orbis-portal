6ce5149753932a2ade9cdcd3320d62888a40b189
tsukikage-R8 <msunaneko@gmail.com>
2026-08-14 13:54:43 +0000

    feat: タレント別イントロFXを9名分それぞれ強化（専用シーン背景＋クライマックス演出）

--- scripts/gen-pages.js
+++ scripts/gen-pages.js
@@ -48,7 +48,14 @@ function fxHtml(kind) {
       '<span class="fx-heart" style="left:70%;top:16%;animation-delay:1.1s;animation-duration:5.5s"></span>' +
       '<span class="fx-heart" style="left:44%;top:64%;animation-delay:1.7s;animation-duration:6s"></span>' +
       '<span class="fx-spark" style="left:24%;top:40%;animation-delay:0.8s"></span>' +
-      '<span class="fx-spark" style="left:85%;top:55%;animation-delay:1.5s"></span>',
+      '<span class="fx-spark" style="left:85%;top:55%;animation-delay:1.5s"></span>' +
+      '<span class="fx-moon-big"></span>' +
+      '<span class="fx-eyes"><i></i><i></i></span>' +
+      '<span class="fx-paw" style="right:8%;bottom:9%;animation-delay:1.2s"><b></b></span>' +
+      '<span class="fx-paw" style="right:22%;bottom:25%;animation-delay:1.7s;transform:scale(0.8) rotate(14deg)"><b></b></span>' +
+      '<span class="fx-paw" style="right:37%;bottom:41%;animation-delay:2.2s;transform:scale(0.62) rotate(26deg)"><b></b></span>' +
+      '<span class="fx-flash"></span>' +
+      '<span class="fx-fin">MOON HOWL</span>',
     note:
       '<span class="fx-note-i" style="left:12%;animation-delay:0s;font-size:34px">♩</span>' +
       '<span class="fx-note-i" style="left:32%;animation-delay:1.4s;font-size:26px">♪</span>' +
@@ -56,13 +63,25 @@ function fxHtml(kind) {
       '<span class="fx-note-i" style="left:78%;animation-delay:2.1s;font-size:30px">♬</span>' +
       '<span class="fx-note-i" style="left:92%;animation-delay:1s;font-size:24px">♪</span>' +
       '<span class="fx-note-gem" style="left:22%;top:18%;animation-delay:0.4s"></span>' +
-      '<span class="fx-note-gem" style="left:76%;top:32%;animation-delay:1.6s;animation-duration:4s"></span>',
+      '<span class="fx-note-gem" style="left:76%;top:32%;animation-delay:1.6s;animation-duration:4s"></span>' +
+      '<span class="fx-spot"></span>' +
+      '<span class="fx-vinyl"></span>' +
+      '<span class="fx-eq"><i style="animation-delay:0s"></i><i style="animation-delay:0.15s;animation-duration:1.05s"></i>' +
+      '<i style="animation-delay:0.3s"></i><i style="animation-delay:0.45s;animation-duration:0.95s"></i>' +
+      '<i style="animation-delay:0.6s"></i><i style="animation-delay:0.75s;animation-duration:1.1s"></i></span>' +
+      '<span class="fx-flash"></span>' +
+      '<span class="fx-fin">SHINE</span>',
     demon:
       '<span class="fx-bat" style="top:16%"></span>' +
       '<span class="fx-bat" style="top:36%;animation-delay:3.2s;animation-duration:11s;width:88px;opacity:0.7"></span>' +
       '<span class="fx-bat" style="top:62%;animation-delay:5.6s;animation-duration:9.5s;width:128px"></span>' +
       '<span class="fx-flame" style="left:16%;bottom:-16px"></span>' +
-      '<span class="fx-flame" style="left:78%;bottom:-22px;animation-delay:0.8s;width:80px;height:98px"></span>',
+      '<span class="fx-flame" style="left:78%;bottom:-22px;animation-delay:0.8s;width:80px;height:98px"></span>' +
+      '<span class="fx-penta"></span>' +
+      '<span class="fx-flame2" style="left:9%;bottom:-24px"></span>' +
+      '<span class="fx-flame2" style="right:8%;bottom:-30px;animation-delay:1.6s;animation-duration:2s"></span>' +
+      '<span class="fx-flash"></span>' +
+      '<span class="fx-fin">降臨</span>',
     sea:
       '<span class="fx-sun"></span>' +
       '<span class="fx-bub2" style="left:12%;bottom:-30px"></span>' +
@@ -73,8 +92,13 @@ function fxHtml(kind) {
       '<span class="fx-shell" style="left:82%;top:42%;animation-delay:1.4s;animation-duration:7.5s">' + DECO_SVG.shell + "</span>" +
       '<span class="fx-spark" style="left:30%;top:10%;animation-delay:0.5s"></span>' +
       '<span class="fx-spark" style="left:70%;top:14%;animation-delay:1.1s"></span>' +
+      '<span class="fx-ball" style="left:26%;bottom:14%"></span>' +
+      '<span class="fx-deck"><i class="fx-plate"></i><i class="fx-fader"></i></span>' +
+      '<span class="fx-beat"></span>' +
       waveSvg("rgba(255,255,255,0.4)") +
-      waveSvg("rgba(255,255,255,0.22)"),
+      waveSvg("rgba(255,255,255,0.22)") +
+      '<span class="fx-flash"></span>' +
+      '<span class="fx-fin">DJ START</span>',
     deep:
       '<span class="fx-ray" style="left:12%;animation-delay:0s"></span>' +
       '<span class="fx-ray" style="left:55%;animation-delay:1.6s;transform:rotate(-12deg)"></span>' +
@@ -87,7 +111,22 @@ function fxHtml(kind) {
       '<span class="fx-glowdot" style="left:25%;top:30%;animation-delay:0.3s"></span>' +
       '<span class="fx-glowdot" style="left:70%;top:22%;animation-delay:1.2s"></span>' +
       '<span class="fx-glowdot" style="left:48%;top:70%;animation-delay:2s"></span>' +
-      '<span class="fx-glowdot" style="left:88%;top:66%;animation-delay:0.9s;animation-duration:4.5s"></span>',
+      '<span class="fx-glowdot" style="left:88%;top:66%;animation-delay:0.9s;animation-duration:4.5s"></span>' +
+      '<span class="fx-jelly" style="right:9%;top:8%;width:104px">' +
+      '<span class="j-dome"></span>' +
+      '<span class="j-t" style="left:24%;height:44px;animation-delay:0.2s"></span>' +
+      '<span class="j-t" style="left:42%;height:60px;animation-delay:0.8s;animation-duration:2.2s"></span>' +
+      '<span class="j-t" style="left:60%;height:52px;animation-delay:1.3s"></span>' +
+      '<span class="j-t" style="left:78%;height:40px;animation-delay:1.8s;animation-duration:3s"></span>' +
+      "</span>" +
+      '<span class="fx-jelly" style="left:7%;bottom:12%;width:64px;opacity:0.65;animation-delay:3.4s;animation-duration:13s">' +
+      '<span class="j-dome" style="height:40px"></span>' +
+      '<span class="j-t" style="left:26%;height:30px;animation-delay:0.4s"></span>' +
+      '<span class="j-t" style="left:52%;height:42px;animation-delay:1s"></span>' +
+      '<span class="j-t" style="left:74%;height:28px;animation-delay:1.6s;animation-duration:2.2s"></span>' +
+      "</span>" +
+      '<span class="fx-flash"></span>' +
+      '<span class="fx-fin">安らぎ</span>',
     koma:
       '<span class="fx-koma-spin" style="left:10%;top:10%;width:96px">' + DECO_SVG.top + "</span>" +
       '<span class="fx-koma-spin" style="right:8%;bottom:6%;width:72px;animation-duration:4.2s">' + DECO_SVG.top + "</span>" +
@@ -95,7 +134,13 @@ function fxHtml(kind) {
       '<i class="fx-d-h"></i><i class="fx-d-b" style="--i:1"></i><i class="fx-d-b" style="--i:2"></i>' +
       '<i class="fx-d-b" style="--i:3"></i><i class="fx-d-b" style="--i:4"></i></span>' +
       '<span class="fx-seigaiha" style="left:6%;bottom:2%;width:170px;height:170px"></span>' +
-      '<span class="fx-seigaiha" style="right:16%;bottom:24%;width:110px;height:110px;animation-duration:44s"></span>',
+      '<span class="fx-seigaiha" style="right:16%;bottom:24%;width:110px;height:110px;animation-duration:44s"></span>' +
+      '<span class="fx-lantern" style="left:8%;top:10%"></span>' +
+      '<span class="fx-lantern" style="left:16%;top:20%;animation-delay:1.4s;animation-duration:7s"></span>' +
+      '<span class="fx-hanabi" style="left:60%;top:22%"></span>' +
+      '<span class="fx-hanabi fx-hana2" style="left:22%;top:36%;width:88px;height:88px;animation-delay:1.1s"></span>' +
+      '<span class="fx-flash"></span>' +
+      '<span class="fx-fin">見参！</span>',
     sleep:
       '<span class="fx-cloud" style="top:12%;animation-duration:26s"></span>' +
       '<span class="fx-cloud" style="top:30%;animation-delay:8s;animation-duration:32s;opacity:0.5;width:150px"></span>' +
@@ -103,7 +148,15 @@ function fxHtml(kind) {
       '<span class="fx-moon"><svg viewBox="0 0 100 100" aria-hidden="true"><path fill="#fff" d="M70,10 A46,46 0 1 0 70,90 A34,34 0 0 1 70,10Z"/></svg></span>' +
       '<span class="fx-zzz" style="left:18%;bottom:16%;animation-delay:0s">Z</span>' +
       '<span class="fx-zzz" style="left:28%;bottom:26%;animation-delay:1.2s;font-size:1.5rem">z</span>' +
-      '<span class="fx-zzz" style="left:36%;bottom:34%;animation-delay:2.3s;font-size:1.1rem;animation-duration:5s">z</span>',
+      '<span class="fx-zzz" style="left:36%;bottom:34%;animation-delay:2.3s;font-size:1.1rem;animation-duration:5s">z</span>' +
+      '<span class="fx-star-shoot"></span>' +
+      '<span class="fx-spark" style="left:12%;top:18%;animation-delay:0.4s"></span>' +
+      '<span class="fx-spark" style="left:86%;top:22%;animation-delay:0.9s"></span>' +
+      '<span class="fx-spark" style="left:70%;top:60%;animation-delay:1.5s"></span>' +
+      '<span class="fx-spark" style="left:26%;top:52%;animation-delay:2s;width:12px;height:12px"></span>' +
+      '<span class="fx-pillow"></span>' +
+      '<span class="fx-flash"></span>' +
+      '<span class="fx-fin">おやすみ</span>',
     rain:
       '<span class="fx-rain-s" style="left:8%;animation-delay:0s"></span>' +
       '<span class="fx-rain-s" style="left:22%;animation-delay:0.6s;animation-duration:1.9s"></span>' +
@@ -116,7 +169,11 @@ function fxHtml(kind) {
       '<span class="fx-rip" style="left:55%;bottom:10%;animation-delay:1.2s"></span>' +
       '<span class="fx-rip" style="left:82%;bottom:6%;animation-delay:2.2s;animation-duration:3.8s"></span>' +
       '<span class="fx-puddle" style="left:30%;bottom:-6px"></span>' +
-      '<span class="fx-puddle" style="left:70%;bottom:-2px;animation-delay:0.9s"></span>',
+      '<span class="fx-puddle" style="left:70%;bottom:-2px;animation-delay:0.9s"></span>' +
+      '<span class="fx-umbrella"><svg viewBox="0 0 120 110" aria-hidden="true"><path d="M14 58a46 46 0 0 1 92 0z" fill="#3d4a55"/><path d="M14 58a46 46 0 0 1 92 0" fill="none" stroke="#5a6a78" stroke-width="2.5"/><g stroke="#54626f" stroke-width="3" stroke-linecap="round"><path d="M34 59L60 92 86 59"/><path d="M60 92v12"/></g><path d="M53 104q7 11 14 0" fill="none" stroke="#ef8b8b" stroke-width="4" stroke-linecap="round"/></svg></span>' +
+      '<span class="fx-bolt"><svg viewBox="0 0 120 260" aria-hidden="true"><polygon points="72,0 26,140 62,140 38,260 100,108 62,108 88,0" fill="#ffe9a8"/></svg></span>' +
+      '<span class="fx-flash2"></span>' +
+      '<span class="fx-fin">雨宿り</span>',
     paint:
       '<svg class="fx-rainbow-svg" viewBox="0 0 340 170" aria-hidden="true"><path stroke="#ff8fab" stroke-width="22" d="M20,170 A150,150 0 0 1 320,170"/><path stroke="#ffd166" stroke-width="20" d="M40,170 A130,130 0 0 1 300,170"/><path stroke="#6bc77b" stroke-width="18" d="M60,170 A110,110 0 0 1 280,170"/><path stroke="#4d96ff" stroke-width="16" d="M80,170 A90,90 0 0 1 260,170"/><path stroke="#b980f0" stroke-width="14" d="M100,170 A70,70 0 0 1 240,170"/></svg>' +
       '<span class="fx-splat" style="left:15%;top:14%;background:#ff7aa2;animation-delay:0.2s"></span>' +
@@ -127,7 +184,13 @@ function fxHtml(kind) {
       '<span class="fx-drip" style="left:63%;background:#8bd3dd;animation-delay:1.6s"></span>' +
       '<span class="fx-drip" style="left:82%;background:#ffd166;animation-delay:3s;height:34px"></span>' +
       '<span class="fx-spark" style="left:40%;top:26%;animation-delay:0.4s"></span>' +
-      '<span class="fx-spark" style="left:58%;top:60%;animation-delay:1.3s;width:20px;height:20px"></span>',
+      '<span class="fx-spark" style="left:58%;top:60%;animation-delay:1.3s;width:20px;height:20px"></span>' +
+      '<span class="fx-blob" style="left:30%;top:16%;background:#ff8fab;animation-delay:1.9s"></span>' +
+      '<span class="fx-blob" style="left:55%;top:12%;background:#ffd166;animation-delay:2.14s;width:14px;height:14px"></span>' +
+      '<span class="fx-blob" style="left:42%;top:30%;background:#4d96ff;animation-delay:2.4s;width:12px;height:12px"></span>' +
+      '<span class="fx-blob" style="left:66%;top:42%;background:#6bc77b;animation-delay:2.7s;width:20px;height:20px"></span>' +
+      '<span class="fx-flash"></span>' +
+      '<span class="fx-fin">ぬふぬふ！</span>',
     lockon:
       '<div class="fx-grid"></div>' +
       '<span class="fx-scan"></span>' +
